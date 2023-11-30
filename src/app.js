e65ca9770aa14d2bcb50f4389c41464c21fdd6bb
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-30 14:29:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.patch('/api/v1/products/:id', (req, res) => {
     else if(product.quantity == 0){
         res.status(404).json({
             status : "success",
-            message : "${product.name}, Out of stock!"
+            message : `${product.name}, Out of stock!`
         })
     }
     else {
